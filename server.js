8f6f842595edf221edb371e14da8c6d8cf770e96
twalt007 <tatumwalter@gmail.com>
2020-02-01 21:12:23 -0800

    simple get request working.  server lives!

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
-const PORT = proccess.env.PORT || 3000;
 const db = require('./db');
 const app = express();
 const path = require('path');
+const PORT = process.env.PORT || 3000;
 
 
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.get('/get-request', async (req, res)=>{
+    const [ result ] = await db.query('SELECT * FROM posts');
+    res.send({
+        posts: result
+    });
+});
+
+app.listen(PORT, () => {
+    console.log("Server is listenint @ localhost: " + PORT)
+});
