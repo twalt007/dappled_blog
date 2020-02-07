d4f0cc676e749bd97c593a61e4f460a3e96b96a1
twalt007 <tatumwalter@gmail.com>
2020-02-06 19:58:32 -0800

    created admin home elements and css; got basic button re-route functionality to work

--- client/src/components/admin/general/footer/admin_footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const AdminFooter = props => {
-    return(
-        <div>AdminFooter</div>
-    )
-}
-
-export default AdminFooter
--- client/src/components/admin/home/admin_home.js
+++ client/src/components/admin/home/admin_home.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import AdminHeader from '../general/header'
+import './admin_home.scss'
 
 const AdminHome = props => {
+    function createNewPost(){
+        console.log('function syntax worked');
+        props.history.push(`/new`)
+    }
     return(
-        <div>
+        <div className = "admin-home-container">
             <AdminHeader />
-            AdminHome
+            <div className="home-text">
+                <h1 className="h1 text">Welcome to your happy place!</h1>
+                <h2 className="h2 text">What would you like to do?</h2>
+            </div>
+            
+            <div className="function-buttons">
+                <div className = "button-container">
+                    <div className = "large-button fat-border button-text" onClick={createNewPost} >Create New Post</div>                
+                </div>
+                <div className = "button-container">
+                    <div className = "large-button fat-border button-text">Edit Post</div>                
+                </div> 
+                <div className = "button-container">
+                    <div className = "large-button fat-border button-text">Delete Post</div>                
+                </div>          
+            </div>
+            <div className = "button-container">
+                <div className = "fat-border small-button right bottom button-text">View</div>                
+            </div>
         </div>
+        
     )
 }
 
