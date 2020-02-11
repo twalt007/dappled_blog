d3ea9ecb71c9b72b215e523a66829158d5838e5f
twalt007 <tatumwalter@gmail.com>
2020-02-10 23:23:31 -0800

    submit button corrected; basic css applied

--- client/src/components/admin/new/new.js
+++ client/src/components/admin/new/new.js
@@ -5,12 +5,17 @@ import PostForm from '../general/form/postForm'
 
 const NewPost = props => {
     const {history} = props;
+    const handleSubmit = (values) => {
+        console.log("inside handleSubmit Funciton");
+        history.push('/success');
+        console.log("form values: ", values);
+    }
     const initialValues = {postTitle:'', postContent: '', postQuote: ''}
     return (
         <div className="section-container">
             <AdminHeader />
             <NavButton text="Create New Post" buttonClasses = "title center" onClick="null"/>
-            <PostForm mainHistory={history} text="Post" initialValues={initialValues}/>
+            <PostForm handleSubmit={handleSubmit} mainHistory={history} text="Post" initialValues={initialValues}/>
         </div>
     )
 }
