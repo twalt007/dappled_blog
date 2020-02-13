04ff14619df7a7f79b19a2b258967717309e9de1
twalt007 <tatumwalter@gmail.com>
2020-02-12 22:58:10 -0800

    client and server now communicating; fixed glitches with userId and posttype validation/preset

--- client/src/components/admin/new/new.js
+++ client/src/components/admin/new/new.js
@@ -6,25 +6,26 @@ import axios from 'axios';
 
 const NewPost = props => {
     const {history} = props;
- 
-    const handleSubmit = (values) => {
+    const userId = 'a9ec5c8d-455a-11ea-8fd0-a4db300c2566';
+    const handleSubmit = async(values) => {
         console.log("inside handleSubmit Funciton");
-        history.push('/success');
         console.log("form values: ", values);
         //need to get userId from elsewhere; can't hardcode
-        const userId = 'a9ec5c8d-455a-11ea-8fd0-a4db300c2566';
         const data = {
             userId: userId,
             post: {
+                postType: values.postType,
+                contentType: values.contentType,
                 postTitle: values.postTitle,
                 postContent: values.postContent,
                 postQuote: values.postQuote,
             }
         }
         try{
-            const resp = await axios.post(`/api/admin/new-post`, data, axiosConfig);
+            const resp = await axios.post(`/api/admin/new-post`, data);
             console.log("hanleSubmit resp from axios call: ", resp);
-            return ({"Congratulations! Your content was successfully posted!"})
+            history.push('/success');
+            return ("Congratulations! Your content was successfully posted!");
         }
         catch (error){
             console.log("Error submitting content to be posted. ", error)
