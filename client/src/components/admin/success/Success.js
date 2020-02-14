7e83425d7229195dd02a1c952df8108e06bd9dc2
twalt007 <tatumwalter@gmail.com>
2020-02-13 22:13:09 -0800

    Success Page Created and Styled

--- client/src/components/admin/success/Success.js
+++ client/src/components/admin/success/Success.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import AdminHeader from '../general/header'
+import NavButton from '../general/navButton'
+
 
 const Success = props => {
+    const {history} = props
     return (
-        <div>Success Note!</div>
+        <div className = "section-container">
+            <AdminHeader />
+            <NavButton text="Success!" buttonClasses = "title center" onClick="null"/>
+            <div className = "green-space">
+                <h4 className="h4 center">Congratulations! Action successfully completed!</h4>
+                <h4 className="h4 center">You can rest in peace knowing this was done.</h4>
+            </div>
+            <div className="flexed">
+                <NavButton buttonClasses='small-button' text='Admin Home' url='/' mainHistory={history}/>
+            </div>
+
+
+        </div>
     )
 }
 
-export default Success
+export default Success;
