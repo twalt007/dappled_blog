491e3fc9de0d43e0667c8ce2261b35c16490a146
twalt007 <tatumwalter@gmail.com>
2020-02-15 10:43:12 -0800

    tested a few items.  no change made

--- client/src/components/admin/edit/editListItem.js
+++ client/src/components/admin/edit/editListItem.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
-const EditListItem = () => {
+const EditListItem = (props) => {
+    const {onClick} = props;
     return(
-        <div className='list-item'>
+        <div className='list-item' onClick={onClick}>
             <h5 className="h5 edit-list-title">Post Title</h5>
             <h6 className="h6 indent italicized edit-list-date">Last Updated Month ##, ####</h6>
         </div> 
--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
-const routes = require('./routes');
+const ApiError = require('./helpers/api_error');
+global.ApiError = ApiError;
+const routes = require('./routes')
 const path = require('path');
+const defaultErrorHandler = require('./middleware/default_error_handler');
+
 
 const app = express();
 
-
-app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'client','dist')));
 
 app.use(routes);
 
-app.get('/get-request', async (req, res)=>{
-    const [ result ] = await db.query('SELECT * FROM posts');
-    res.send({
-        posts: result
-    });
-});
+app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'client','dist','index.html'));
+})
+
+app.use(defaultErrorHandler);
 
 app.listen(PORT, () => {
-    console.log("Server is listenint @ localhost: " + PORT)
+    console.log("Server is listening @ localhost: " + PORT)
 });
