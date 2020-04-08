edfa40ab053f2c42af0ef20cc26018b9abb3ce53
twalt007 <tatumwalter@gmail.com>
2020-04-07 23:14:36 -0700

    abandoning class component method and going with mazzy's method

--- client/src/components/admin/general/form/form.js
+++ client/src/components/admin/general/form/form.js
@@ -47,189 +47,19 @@ class Form extends Component {
 
     };
 
-    render (){
-        // const { initialValues, text= 'ok', mainHistory, returnUrl= '/'} = this.props //handleSubmit 
-        console.log("this.props.children", this.props.children);
-        // <form className="form" encType="multipart/form-data" onSubmit={handleSubmit}>
-        return(
-            <form className="form" encType="multipart/form-data">
-                {this.props.children}
-            </form>
-        )
+    // render (){
+    //     const { initialValues, text= 'ok', mainHistory, returnUrl= '/', reroute} = this.props //handleSubmit 
+    //     console.log("this.props.children", this.props.children);
+    //     return(
+    //         <form className="form" encType="multipart/form-data" onSubmit={this.handleSubmit}>
+    //             {this.props.children}
+    //         </form>
+    //     )
         
-    }        
+    // }        
 }
-
-
-// const postList = postData.map((post,index) => {
-//     return(
-//         <PostTile 
-//             key={index}
-//             {...post}
-//             onClick={this.goToDetails.bind(this, post.postId)}
-//         />
-//     )
-// });
-// return(
-//         <div className = "client-container blog-container">
-//             <BlogHeader mainHistory={this.history}/>
-//             <div className="welcome-text green-space">
-//                 <h1 className="text center">Welcome to Dappled!</h1>
-//             </div>                
-//             <div className="tile-list center-margins">
-//                 {postList}
-//             </div>
-//             <BlogFooter mainHistory={history} name={name}/>
-//         </div>
-// );
-
 
 
 export default Form;
 
-//remove ids
-//create new componenet for file input
-
-
-// import React, { Component } from 'react';
-// import * as Yup from 'yup';
-// import './postForm.scss'
-
-// class NameForm extends React.Component {
-//     constructor(props) {
-//       super(props);
-//       this.state = {value: ''};
-  
-//       this.handleChange = this.handleChange.bind(this);
-//       this.handleSubmit = this.handleSubmit.bind(this);
-//     }
-  
-//     handleChange(event) {
-//       this.setState({value: event.target.value});
-//     }
-  
-//     handleSubmit(event) {
-//       alert('A name was submitted: ' + this.state.value);
-//       event.preventDefault();
-//     }
-  
-//     render() {
-//       return (
-//         <form onSubmit={this.handleSubmit}>
-//           <label>
-//             Name:
-//             <input type="text" value={this.state.value} onChange={this.handleChange} />
-//           </label>
-//           <input type="submit" value="Submit" />
-//         </form>
-//       );
-//     }
-//   }
-
-
-// class PostForm extends Component {
-//     constructor(props){
-//         super(props);
-//         this.state = {
-//             data: {},
-//             errors: {}
-//         };
-
-//         this.handleChange = this.handleChange.bind(this);
-//         this.handleSubmit = this.handleSubmit.bind(this);
-//     }
-
-//     handleChange(e){
-//         this.setState({
-//             data: event.target.value
-//         });
-//     }
-
-//     handleSubmit(e) {
-//         //do work
-//         e.prevenntDefault();
-//     }
-
-//     reroute(){mainHistory.push(returnnUrl)};
-
-//     render() {
-//         const {initialValues, text='Ok', mainHistory, handleSubmit, returnUrl='/'} = props;
-
-//     }
-// }(props) => {
-//     function reroute(){
-//         mainHistory.push(returnUrl);
-//     }
-//     return (
-//         <Formik 
-//             initialValues={initialValues}
-            
-//             validationSchema={Yup.object({
-//                 postTitle: Yup.string()
-//                     .required('Please provide a title. Don\'t forget to name your latest brainchild!')
-//                     .max(60,'Title too long; will not fit on tile.  Please limit to 60 characters.'),
-//                 postContent: Yup.string()
-//                     .required('Please provide content. You\'ve got readers chomping at the bit to see what you have to say - c\'mon, thow them a bone!'),
-//                 postQuote: Yup.string()
-//                     .required('Please provide a quote to spark readers\' interest - preferably something witty.')
-//                     .max(255,'Sorry - too long!  There is a difference between a quote and a post you know!'),
-//                 postImage: Yup.mixed('Unsupported file type loaded. Please double check!')               
-//             })}
-            
-//             onSubmit = {(values, {setSubmitting})=>{
-//                 handleSubmit(values);
-//                 setSubmitting(false);
-//             }}
-//         >
-//             <Form className="form" encType="multipart/form-data">
-//                 <label className="form-label" htmlFor="postTitle">Post Title</label>
-//                 <Field className="fat-border form-input" name="postTitle" type="text" />
-//                 <ErrorMessage className="form-error" name="postTitle" />
-//                 <label className="form-label" htmlFor="postContent">Post Content</label>
-//                 <Field className="fat-border form-textarea" name="postContent" type="text" component="textarea" />
-//                 <ErrorMessage className="form-error" name="postContent" />
-//                 <label className="form-label" htmlFor="postQuote">Post Quote</label>
-//                 <Field className="fat-border form-input" name="postQuote" type="text" />
-//                 <ErrorMessage className="form-error" name="postQuote" />
-//                 <label className="form-label" htmlFor="postImage">Upload Image</label>
-//                 <Field className="fat-border form-input" id="postImage" name="postImage" type="file" accept="image/*"></Field>
-//                 <ErrorMessage className="form-error" name="postImage" />
-//                 <div className="xlrg-flx-container flex-right">
-//                     <div className="lrg-container align-right top-space">
-//                         <div className = 'small-button fat-border after-space' onClick={reroute}>Return</div>                
-//                         <button className='text small-button fat-border' type='submit'>{text}</button>
-//                     </div>
-//                 </div>
-//             </Form>
-//         </Formik>
-//     )
-// }
-
-
-// export default PostForm;
-
-
-
-
-//just trying to see what types of things are needed to make this form work.
-
-//trying to allow the pieces to still be able to recieve the stuff they will need - inputs should be able to recieve all  initial values
-//think goal will just be to create a basic form that works
-//after that, when undersatnd it better, can look into ways to tweak it so that I don't hvae to recode it all over again for the u-auth system
-//wtrying to see if I can get rid of the reroute function, and have it still work in my navbutton pieces, as well as here
-
-//basic needed pieces:
-
-// handleChange   
-//     setState
-// handleSubmit
-//     prevent default
-//     validate
-//         stop with one error/validate on unfocus
-//     make axios call or whatever I do
-
-// submit button grey until no errors
-// can't have more than one error show, unless for logins
-
-//meta touched && ...
-//reset props??
+//{React.cloneElement(this.props.children, {...this.props})}
--- client/src/components/admin/new/new.js
+++ client/src/components/admin/new/new.js
@@ -5,9 +5,22 @@ import Form from '../general/form'
 import { Field, FormButton } from '../general/form/formComponents'
 import axios from 'axios';
 
-const NewPost = props => {
-    const {history, userId='a9ec5c8d-455a-11ea-8fd0-a4db300c2566'} = props;
-    const handleSubmit = async(values) => {
+class NewPost extends Form(){
+    constructor(props){
+        super(props);
+
+        this.state={
+            data: {},
+            errors:{},
+        }
+
+        this.history = props.history;
+        this.handleSubmit = this.handleSumbit.bind(this);
+
+    }
+
+    handleSubmit = async(values) => {
+        const {userId='a9ec5c8d-455a-11ea-8fd0-a4db300c2566'} = this.props;
         console.log("inside handleSubmit");
         // const data = {
         //     userId: userId,
@@ -34,28 +47,28 @@ const NewPost = props => {
         //     console.log("Error submitting content to be posted. ", error);
         //     history.push('/result-message');
         // }   
-    }
+    };
     
-    const initialValues = {postTitle:'', postContent: '', postQuote: ''}
-
-    return (
-        <div className="admin section-container center">
-            <div className="admin-background">
-                <AdminHeader mainHistory={history}/>
-                <NavButton text="Create New Post" buttonClasses = "title" onClick="null"/>
-                {/* <Form handleSubmit={handleSubmit} mainHistory={history} text="Post" initialValues={initialValues}> */}
-                <Form >
-                    <Field name="testingDefault" label="Testing Default" />,
-                    <Field name="testingTextArea" label="testing Text Area" fieldClass="textarea" />,
-                    {/* <Field name="testingOnChange" label="testing On Change" onChange={handleChange} />,
-                    <Field name="testingImage" label="testingimage" type="file" />,
-                    <FormButton returnText="ReturnTest" text={text} reroute={reroute}/> */}
-                </Form>
-                {/* <PostForm handleSubmit={handleSubmit} mainHistory={history} text="Post" initialValues={initialValues}/> */}
-                <div className="bottom-space"></div>
+   render(){
+       return(
+            <div className="admin section-container center">
+                <div className="admin-background">
+                    <AdminHeader mainHistory={history}/>
+                    <NavButton text="Create New Post" buttonClasses = "title" onClick="null"/>
+                    {/* should go below  mainHistory={history} initialValues={initialValues}*/}
+                    <form handleSubmit={this.handleSubmit} >  
+                        <Field name="testingDefault" label="Testing Default" />,
+                        <Field name="testingTextArea" label="testing Text Area" fieldClass="textarea" />
+                        <Field name="testingOnChange" label="testing On Change" onChange={this.handleChange} />
+                        <Field name="testingImage" label="testingimage" type="file" /> */} 
+                        {/* <FormButton returnText="Return" text="Post" reroute={this.reroute}/>  */}
+                    </form>
+                    {/* <PostForm handleSubmit={handleSubmit} mainHistory={history} text="Post" initialValues={initialValues}/> */}
+                    <div className="bottom-space"></div>
+                </div>
             </div>
-        </div>
-    )
+       );
+    };
 }
 
 export default NewPost;
