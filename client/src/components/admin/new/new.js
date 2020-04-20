ff2ebe1ee076c1a4f9e5a6c5f309a55114e824d0
twalt007 <tatumwalter@gmail.com>
2020-04-19 20:23:28 -0700

    added custom form code from form-sandbox dev repo

--- client/src/components/admin/new/new.js
+++ client/src/components/admin/new/new.js
@@ -1,73 +1,135 @@
-import React from 'react'
+import React, { Component } from 'react'
 import AdminHeader from '../general/header/adminHeader'
 import NavButton from '../../general/navButton'
-import Form from '../general/form/form'
 import { Field, FormButton } from '../general/form/formComponents'
+import * as yup from 'yup'
 import axios from 'axios';
 
-class NewPost extends Form(){
+class NewPost extends Component {
     constructor(props){
-        super(props);
+        super(props)
 
-        this.state={
+        this.state = {
             data: {},
-            errors:{},
+            errors: {},
+            initVal: {}
         }
 
-        this.history = props.history;
-        this.handleSubmit = this.handleSumbit.bind(this);
+        this.validSchema = {
+            postTitle: yup.string()
+            .required('Please provide a title. Don\'t forget to name your latest brainchild!')
+            .max(60,'Title too long; will not fit on tile.  Please limit to 60 characters.')
+            .trim(),
+            postContent: yup.string()
+            .required('Please provide content. Your audience awaits!')
+            .trim(),
+            postQuote: yup.string()
+            .required('Please provide a quote to spark readers\' interest - preferably something witty.')
+            .max(255,'Sorry - too long!  There is a difference between a quote and a post you know!')
+            .trim()
+        }
 
+        this.reroute = this.reroute.bind(this);
+        this.validateForm = this.validateForm.bind(this);
+        this.validateField = this.validateField.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChangeBlur = this.handleChangeBlur.bind(this);
+        this.submitForm = this.submitForm.bind(this);
+    }        
+
+    reroute(){
+        this.props.history.goBack();
+    };
+
+    async validateForm(){
+        const data = {...this.state};
+        const schema = yup.object().shape(this.validSchema);
+        console.log("validateForm starting state: ", data)
+        let errors = {};
+        await schema.validate(this.state.data, {abortEarly:false}).catch(errs => {
+            errs.inner.map(err=>{
+                errors[err.path] = err.message;
+            });            
+        });
+        return errors;
+    };
+
+    async validateField({name, value }){
+        const schema = yup.object().shape({ [name]: this.validSchema[name] });
+        let obj = {[name]:value};
+        let errorMessage = await schema.validate(obj).catch(errs => errs);
+        if (errorMessage.message) {
+            return errorMessage.message
+        } else return null;
+    };
+
+    async handleSubmit(e){
+        e.preventDefault();
+
+        let allErrors = await this.validateForm();
+        this.setState({errors : allErrors });
+
+        if (Object.keys(allErrors).length !== 0) {
+            return;
+        };
+
+        this.submitForm(this.state.data);
+    };
+
+    async handleChangeBlur({currentTarget: input}){
+        const errors = {...this.state.errors};
+
+        let errorMessage = await this.validateField(input);
+        if (errorMessage) errors[input.name] = errorMessage;
+        else delete errors[input.name];
+
+        const data = { ...this.state.data };
+        data[input.name] = input.value;
+        this.setState({ data, errors });
+    };
+
+    submitForm = async(values) => {
+        const {history, userId='a9ec5c8d-455a-11ea-8fd0-a4db300c2566'} = this.props;
+        console.log("newPost handleSubmit form Values: ", values)
+        const data = {
+            userId: userId,
+            post: values 
+        }
+        let resultMessageState;
+        try{
+            const resp = await axios.post(`/api/admin/new-post`, data);
+            if (resp.data.code===200){
+                resultMessageState = 'success';         
+            }
+            history.push('/result-message', resultMessageState);
+            return;
+        }
+        catch (error){
+            console.log("Error submitting content to be posted. ", error);
+            history.push('/result-message');
+        }      
+        
     }
 
-    handleSubmit = async(values) => {
-        const {userId='a9ec5c8d-455a-11ea-8fd0-a4db300c2566'} = this.props;
-        console.log("inside handleSubmit");
-        // const data = {
-        //     userId: userId,
-        //     post: {
-        //         postType: values.postType,
-        //         contentType: values.contentType,
-        //         postTitle: values.postTitle,
-        //         postContent: values.postContent,
-        //         postQuote: values.postQuote,
-        //     }
-        // }
-        // console.log("handleSubmit new post data: ", data);
-        // let resultMessageState;
-        // try{
-        //     const resp = await axios.post(`/api/admin/new-post`, data);
-        //     console.log("hanleSubmit resp from axios call: ", resp);
-        //     if (resp.data.code===200){
-        //         resultMessageState = 'success';         
-        //     }
-        //     history.push('/result-message', resultMessageState);
-        //     return;
-        // }
-        // catch (error){
-        //     console.log("Error submitting content to be posted. ", error);
-        //     history.push('/result-message');
-        // }   
-    };
-    
-   render(){
-       return(
+    render(){
+        let oCB = this.handleChangeBlur;
+        let err = this.state.errors;
+        return (
             <div className="admin section-container center">
                 <div className="admin-background">
                     <AdminHeader mainHistory={history}/>
                     <NavButton text="Create New Post" buttonClasses = "title" onClick="null"/>
-                    {/* should go below  mainHistory={history} initialValues={initialValues}*/}
-                    <form handleSubmit={this.handleSubmit} >  
-                        <Field name="testingDefault" label="Testing Default" />,
-                        <Field name="testingTextArea" label="testing Text Area" fieldClass="textarea" />
-                        <Field name="testingOnChange" label="testing On Change" onChange={this.handleChange} />
-                        <Field name="testingImage" label="testingimage" type="file" /> */} 
-                        {/* <FormButton returnText="Return" text="Post" reroute={this.reroute}/>  */}
+                    <form className="form" encType="multipart/form-data" onSubmit={this.handleSubmit}>
+                        <Field name='postTitle' label="Post Title" max="60" min="1" error={err} onChange={oCB} onBlur={oCB} />
+                        <Field name="postContent" label="Post Content" fieldClass="textarea" min="1" error={err} onChange={oCB} onBlur={oCB}/>
+                        <Field name="postQuote" label="Post Quote" max="255" min="1" error={err} onChange={oCB} onBlur={oCB} />
+                        <Field name="postImage" label="Post Image" type="file" accept="image/*" error={err} onChange={oCB} onBlur={oCB} />
+                        <FormButton text="Post" reroute={this.reroute}/>
                     </form>
-                    {/* <PostForm handleSubmit={handleSubmit} mainHistory={history} text="Post" initialValues={initialValues}/> */}
                     <div className="bottom-space"></div>
                 </div>
             </div>
-       );
+        )
     };
 }
 
